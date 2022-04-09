aa12c6ad36e7b286aeed896359f60ad07ca79cf5
JavierIgnaciojlr <lopez.rivas.javier@gmail.com>
2022-04-09 13:09:00 -0400

    second commit

--- pokemones.sql
+++ pokemones.sql
@@ -8,8 +8,13 @@ pokedex SERIAL,
 name VARCHAR(50),
 type1 VARCHAR(50),
 type2 VARCHAR(50),
-PRIMARY KEY(pokedex)
 );
 SELECT * FROM pokemones;
 \copy pokemones FROM 'pokemones.csv' csv header;
 SELECT * FROM pokemones;
+
+SELECT name AS nombres, pokedex AS numero FROM pokemones;
+SELECT pokedex, name FROM pokemones WHERE name = 'Squirtle';
+SELECT * FROM pokemones WHERE type1 = 'Fire';
+SELECT * FROM pokemones WHERE type1 = 'Normal';
+SELECT * FROM pokemones WHERE type1 = 'Normal' AND type2 = 'Flying';
