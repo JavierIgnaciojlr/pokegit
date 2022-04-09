46c9c47a13a7f70d31bd781b11809c8261fe3e0c
JavierIgnaciojlr <lopez.rivas.javier@gmail.com>
2022-04-09 12:57:30 -0400

    first commit

---
+++ pokemones.sql
@@ -0,0 +1,15 @@
+\c jlopez
+DROP DATABASE pokemones;
+CREATE DATABASE  pokemones;
+\c pokemones; 
+
+CREATE TABLE pokemones(
+pokedex SERIAL,
+name VARCHAR(50),
+type1 VARCHAR(50),
+type2 VARCHAR(50),
+PRIMARY KEY(pokedex)
+);
+SELECT * FROM pokemones;
+\copy pokemones FROM 'pokemones.csv' csv header;
+SELECT * FROM pokemones;
